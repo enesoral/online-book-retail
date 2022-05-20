d4599a1318b0e54c4bdfe31325422a7bd6d2cd36
Muhammed Enes Oral <enesoral.seng@gmail.com>
2022-05-20 14:17:09 +0300

    Create endpoint for updating stock

--- src/main/java/com/enesoral/bookretail/book/BookController.java
+++ src/main/java/com/enesoral/bookretail/book/BookController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,5 +21,10 @@ class BookController {
     @PostMapping
     public ResponseEntity<Book> save(@Valid @RequestBody BookCommand recordCommand) {
         return new ResponseEntity<>(bookService.save(recordCommand), HttpStatus.OK);
+    }
+
+    @PutMapping("/stocks")
+    public ResponseEntity<BookCommand> update(@Valid @RequestBody StockUpdateCommand stockUpdateCommand) {
+        return new ResponseEntity<>(bookService.updateStock(stockUpdateCommand), HttpStatus.OK);
     }
 }
