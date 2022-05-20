529937de6d9796d3a3891c39f246f96f19e094f6
Muhammed Enes Oral <enesoral.seng@gmail.com>
2022-05-20 23:18:49 +0300

    Create endpoint for saving user

---
+++ src/main/java/com/enesoral/bookretail/user/UserController.java
@@ -0,0 +1,24 @@
+package com.enesoral.bookretail.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> save(@Valid @RequestBody UserCommand userCommand) {
+        return new ResponseEntity<>(userService.save(userCommand), HttpStatus.OK);
+    }
+}
