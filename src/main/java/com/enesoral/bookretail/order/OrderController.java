6310e4897e34237e10780d0ed426c5c36dc05ad8
Muhammed Enes Oral <enesoral.seng@gmail.com>
2022-05-21 12:01:58 +0300

    Validate PathVariable

--- src/main/java/com/enesoral/bookretail/order/OrderController.java
+++ src/main/java/com/enesoral/bookretail/order/OrderController.java
@@ -3,6 +3,7 @@ package com.enesoral.bookretail.order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,16 +12,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 
 @RestController
 @RequestMapping("/orders")
 @RequiredArgsConstructor
+@Validated
 public class OrderController {
 
     private final OrderService orderService;
 
     @GetMapping("/{orderId}")
-    public ResponseEntity<OrderCommand> getById(@PathVariable String orderId) {
+    public ResponseEntity<OrderCommand> getById(@PathVariable @NotBlank String orderId) {
         return new ResponseEntity<>(orderService.getById(orderId), HttpStatus.OK);
     }
 
