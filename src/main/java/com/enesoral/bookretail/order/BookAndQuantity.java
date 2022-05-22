efbc5b284b50bb685417ac2d921f7a40856cb78e
Muhammed Enes Oral <enesoral.seng@gmail.com>
2022-05-22 22:47:21 +0300

    Add constructors to BookAndQuantity

--- src/main/java/com/enesoral/bookretail/order/BookAndQuantity.java
+++ src/main/java/com/enesoral/bookretail/order/BookAndQuantity.java
@@ -1,8 +1,11 @@
 package com.enesoral.bookretail.order;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
@@ -12,6 +15,8 @@ import javax.validation.constraints.Positive;
 @Setter
 @EqualsAndHashCode
 @Builder
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class BookAndQuantity {
 
     @NotBlank
